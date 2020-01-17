fba97f243ceab56a85e3f6db9d500298ff0181ab
asmirnov69 <asmirnov69@users.noreply.github.com>
2020-01-17 15:27:36 +0000

    more tests

--- tests/fuseki-test.py
+++ tests/fuseki-test.py
@@ -13,6 +13,15 @@ def test():
         print i, df.shape
         #time.sleep(2)
 
+@fuargs.action
+def test_insert(v):
+    conn = fuseki.FusekiConnection("http://localhost:3030/testdb")    
+    rq = 'insert { graph <testdb:shacl-defs> { ?s <testdb:test> "%s" } } where { bind(<testdb:aaa> as ?s) }' % v
+    conn.update(rq)
+    rq = 'select * from <testdb:shacl-defs> { ?s <testdb:test> ?o }'
+    df = conn.select(rq)
+    print df
+        
 if __name__ == "__main__":
     fuargs.exec_actions(sys.argv[1:])
     
